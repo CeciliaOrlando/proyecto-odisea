3555c5c6eae97d7c6c5b88c8c75e989f55dd4572
cecilia orlando <mariaceciliaorlando2021@gmail.com>
2025-05-10 06:47:27 -0300

    asistencia

---
+++ scripts/DeployAsistencia.ts
@@ -0,0 +1,12 @@
+async function main() {
+  //Deployar el contrato
+  //Verificar el contrato
+  //Insertar los addresses de los alumnos
+  //Insertar la primera sesión
+
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exitCode = 1;
+});
